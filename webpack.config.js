fe659afe683c75dc68282cf25e5068cb1a8482c3
Oscar <StickyFancyBoi@Oscars-MacBook-Pro.local>
2016-08-26 09:54:01 -0400

    fix npm env

--- webpack.config.js
+++ webpack.config.js
@@ -10,7 +10,7 @@ module.exports = {
   resolve: {
     extensions: ["", ".js", ".jsx"]
   },
-  'NODE_ENV': "production",
+  NODE_ENV: "production",
   devtool: "source-map",
   module: {
     loaders: [
