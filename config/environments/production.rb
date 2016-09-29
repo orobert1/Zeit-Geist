75edd37281e8d54288f6bb0b2a1da876d4a192cd
Oscar Robert <orobert@risd.edu>
2016-09-28 20:28:06 -0400

    seed database

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -80,7 +80,8 @@ Rails.application.configure do
   :storage => :s3,
       :s3_credentials => {
         :bucket => ENV['zeitgeist-dev'],
-        s3_region: ENV['us-west-2'],
+        :s3_region => ENV['us-west-2'],
+        :region => ENV['us-west-2'],
         :access_key_id => ENV['AKIAJRYVENWGSXVPFSSA'],
         :secret_access_key => ENV['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']
       }
