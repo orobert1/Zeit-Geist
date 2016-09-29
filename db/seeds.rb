0a3dc5b301e389ab32a86d1284bf7c832cee9fbb
Oscar Robert <orobert@risd.edu>
2016-09-28 22:45:35 -0400

    seed db

--- db/seeds.rb
+++ db/seeds.rb
@@ -13,91 +13,91 @@ proj = Project.create(title: "Nike", user_id: User.find_by( username: "batman" )
   Image.create( project_id: Project.find_by( title: "Nike" ).id, project_index: 1, image_file: File.new("#{Rails.root}/images/files/4.jpeg") );
   proj.update(cover_image: cov.image_file.url)
 
-Project.create(title: "Football", user_id: User.find_by( username: "batman" ).id);
+proj = Project.create(title: "Football", user_id: User.find_by( username: "batman" ).id);
 
   cov = Image.create( project_id: Project.find_by( title: "Nike" ).id, project_index: 1, image_file: File.new("#{Rails.root}/images/files/13.jpg") );
   Image.create( project_id: Project.find_by( title: "Nike" ).id, project_index: 1, image_file: File.new("#{Rails.root}/images/files/1.jpg") );
   Image.create( project_id: Project.find_by( title: "Nike" ).id, project_index: 1, image_file: File.new("#{Rails.root}/images/files/4.jpeg") );
   proj.update(cover_image: cov.image_file.url)
 
-Project.create(title: "FunStuff", user_id: User.find_by( username: "batman" ).id);
+proj = Project.create(title: "FunStuff", user_id: User.find_by( username: "batman" ).id);
 
   cov = Image.create( project_id: Project.find_by( title: "Nike" ).id, project_index: 1, image_file: File.new("#{Rails.root}/images/files/14.jpg") );
   Image.create( project_id: Project.find_by( title: "Nike" ).id, project_index: 1, image_file: File.new("#{Rails.root}/images/files/1.jpg") );
   Image.create( project_id: Project.find_by( title: "Nike" ).id, project_index: 1, image_file: File.new("#{Rails.root}/images/files/4.jpeg") );
   proj.update(cover_image: cov.image_file.url)
 
-Project.create(title: "Project 1", user_id: User.find_by( username: "batman" ).id);
+proj = Project.create(title: "Project 1", user_id: User.find_by( username: "batman" ).id);
 
   cov = Image.create( project_id: Project.find_by( title: "Nike" ).id, project_index: 1, image_file: File.new("#{Rails.root}/images/files/15.jpg") );
   Image.create( project_id: Project.find_by( title: "Nike" ).id, project_index: 1, image_file: File.new("#{Rails.root}/images/files/1.jpg") );
   Image.create( project_id: Project.find_by( title: "Nike" ).id, project_index: 1, image_file: File.new("#{Rails.root}/images/files/4.jpeg") );
   proj.update(cover_image: cov.image_file.url)
 
-Project.create(title: "Goodguy", user_id: User.find_by( username: "batman" ).id);
+proj = Project.create(title: "Goodguy", user_id: User.find_by( username: "batman" ).id);
 
   cov = Image.create( project_id: Project.find_by( title: "Nike" ).id, project_index: 1, image_file: File.new("#{Rails.root}/images/files/16.jpg") );
   Image.create( project_id: Project.find_by( title: "Nike" ).id, project_index: 1, image_file: File.new("#{Rails.root}/images/files/1.jpg") );
   Image.create( project_id: Project.find_by( title: "Nike" ).id, project_index: 1, image_file: File.new("#{Rails.root}/images/files/4.jpeg") );
   proj.update(cover_image: cov.image_file.url)
 
-Project.create(title: "Frenchie", user_id: User.find_by( username: "batman" ).id);
+proj = Project.create(title: "Frenchie", user_id: User.find_by( username: "batman" ).id);
 
   cov = Image.create( project_id: Project.find_by( title: "Nike" ).id, project_index: 1, image_file: File.new("#{Rails.root}/images/files/17.jpg") );
   Image.create( project_id: Project.find_by( title: "Nike" ).id, project_index: 1, image_file: File.new("#{Rails.root}/images/files/1.jpg") );
   Image.create( project_id: Project.find_by( title: "Nike" ).id, project_index: 1, image_file: File.new("#{Rails.root}/images/files/4.jpeg") );
   proj.update(cover_image: cov.image_file.url)
 
-Project.create(title: "Prankster", user_id: User.find_by( username: "batman" ).id);
+proj = Project.create(title: "Prankster", user_id: User.find_by( username: "batman" ).id);
 
   cov = Image.create( project_id: Project.find_by( title: "Nike" ).id, project_index: 1, image_file: File.new("#{Rails.root}/images/files/18.jpg") );
   Image.create( project_id: Project.find_by( title: "Nike" ).id, project_index: 1, image_file: File.new("#{Rails.root}/images/files/1.jpg") );
   Image.create( project_id: Project.find_by( title: "Nike" ).id, project_index: 1, image_file: File.new("#{Rails.root}/images/files/4.jpeg") );
   proj.update(cover_image: cov.image_file.url)
 
-Project.create(title: "Love", user_id: User.find_by(username: "batman" ).id);
+proj = Project.create(title: "Love", user_id: User.find_by(username: "batman" ).id);
 
   cov = Image.create( project_id: Project.find_by( title: "Nike" ).id, project_index: 1, image_file: File.new("#{Rails.root}/images/files/19.jpg") );
   Image.create( project_id: Project.find_by( title: "Nike" ).id, project_index: 1, image_file: File.new("#{Rails.root}/images/files/1.jpg") );
   Image.create( project_id: Project.find_by( title: "Nike" ).id, project_index: 1, image_file: File.new("#{Rails.root}/images/files/4.jpeg") );
   proj.update(cover_image: cov.image_file.url)
 
-Project.create(title: "WinterMan", user_id: User.find_by( username: "batman" ).id);
+proj = Project.create(title: "WinterMan", user_id: User.find_by( username: "batman" ).id);
 
   cov = Image.create( project_id: Project.find_by( title: "Nike" ).id, project_index: 1, image_file: File.new("#{Rails.root}/images/files/20.jpg") );
   Image.create( project_id: Project.find_by( title: "Nike" ).id, project_index: 1, image_file: File.new("#{Rails.root}/images/files/1.jpg") );
   Image.create( project_id: Project.find_by( title: "Nike" ).id, project_index: 1, image_file: File.new("#{Rails.root}/images/files/4.jpeg") );
   proj.update(cover_image: cov.image_file.url)
 
-Project.create(title: "AlbumCover", user_id: User.find_by( username: "batman" ).id);
+proj = Project.create(title: "AlbumCover", user_id: User.find_by( username: "batman" ).id);
 
   cov = Image.create( project_id: Project.find_by( title: "Nike" ).id, project_index: 1, image_file: File.new("#{Rails.root}/images/files/21.jpg") );
   Image.create( project_id: Project.find_by( title: "Nike" ).id, project_index: 1, image_file: File.new("#{Rails.root}/images/files/1.jpg") );
   Image.create( project_id: Project.find_by( title: "Nike" ).id, project_index: 1, image_file: File.new("#{Rails.root}/images/files/4.jpeg") );
   proj.update(cover_image: cov.image_file.url)
 
-Project.create(title: "Running", user_id: User.find_by( username: "batman" ).id);
+proj = Project.create(title: "Running", user_id: User.find_by( username: "batman" ).id);
 
   cov = Image.create( project_id: Project.find_by( title: "Nike" ).id, project_index: 1, image_file: File.new("#{Rails.root}/images/files/22.jpg") );
   Image.create( project_id: Project.find_by( title: "Nike" ).id, project_index: 1, image_file: File.new("#{Rails.root}/images/files/1.jpg") );
   Image.create( project_id: Project.find_by( title: "Nike" ).id, project_index: 1, image_file: File.new("#{Rails.root}/images/files/4.jpeg") );
   proj.update(cover_image: cov.image_file.url)
 
-Project.create(title: "Giraffe", user_id: User.find_by( username: "batman" ).id);
+proj = Project.create(title: "Giraffe", user_id: User.find_by( username: "batman" ).id);
 
   cov = Image.create( project_id: Project.find_by( title: "Nike" ).id, project_index: 1, image_file: File.new("#{Rails.root}/images/files/23.jpg") );
   Image.create( project_id: Project.find_by( title: "Nike" ).id, project_index: 1, image_file: File.new("#{Rails.root}/images/files/1.jpg") );
   Image.create( project_id: Project.find_by( title: "Nike" ).id, project_index: 1, image_file: File.new("#{Rails.root}/images/files/4.jpeg") );
   proj.update(cover_image: cov.image_file.url)
 
-Project.create(title: "Broken", user_id: User.find_by( username: "batman" ).id);
+proj = Project.create(title: "Broken", user_id: User.find_by( username: "batman" ).id);
 
   cov = Image.create( project_id: Project.find_by( title: "Nike" ).id, project_index: 1, image_file: File.new("#{Rails.root}/images/files/24.jpg") );
   Image.create( project_id: Project.find_by( title: "Nike" ).id, project_index: 1, image_file: File.new("#{Rails.root}/images/files/1.jpg") );
   Image.create( project_id: Project.find_by( title: "Nike" ).id, project_index: 1, image_file: File.new("#{Rails.root}/images/files/4.jpeg") );
   proj.update(cover_image: cov.image_file.url)
 
-Project.create(title: "Stars", user_id: User.find_by(username: "batman" ).id);
+proj = Project.create(title: "Stars", user_id: User.find_by(username: "batman" ).id);
 
   cov = Image.create( project_id: Project.find_by( title: "Nike" ).id, project_index: 1, image_file: File.new("#{Rails.root}/images/files/25.jpg") );
   Image.create( project_id: Project.find_by( title: "Nike" ).id, project_index: 1, image_file: File.new("#{Rails.root}/images/files/1.jpg") );
