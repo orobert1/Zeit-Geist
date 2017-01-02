96b886a35e7f9ce1f5f35859cb48228808595c9b
Oscar Robert <orobert@risd.edu>
2017-01-01 23:38:15 -0700

    changeProduction.rb

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -74,6 +74,14 @@ Rails.application.configure do
   # Use default logging formatter so that PID and timestamp are not suppressed.
   config.log_formatter = ::Logger::Formatter.new
 
+down vote
+Make sure you set these in your production.rb file
+
+config.cache_classes = true
+config.serve_static_assets = true
+config.assets.compile = true
+config.assets.digest = true
+
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
 
